a675ffead121656934399dc19f3aee1121144d5d
Vaishnavi Panchavati <vaishdho10@gmail.com>
2025-06-23 08:42:04 -0700

    Added deployments and replica creation queue logic

--- src/components/headNode/head_controller.py
+++ src/components/headNode/head_controller.py
@@ -8,13 +8,15 @@ This is where your health checks come.
 '''
 from src.generated import headnode_service_pb2, headnode_service_pb2_grpc, worker_service_pb2_grpc, worker_service_pb2
 import subprocess
-from src.lib import ProxyManager, DeploymentManager, HealthManager, AutoScaleManager, NodeInfo
+from src.lib import ProxyManager, DeploymentManager, HealthManager, AutoScaleManager, NodeInfo, AWSVMManager
 import asyncio
 import grpc 
 from typing import Optional
-from src.lib.helpers import try_send_with_retries
+from src.lib.helpers import try_send_with_retries, get_gcp_internal_ip, get_aws_internal_ip
+from src.lib.utils import get_local_ip
 import argparse
 from concurrent import futures
+import threading
 
 class HeadController(
     headnode_service_pb2_grpc.WorkerManagementServiceServicer,
@@ -25,25 +27,38 @@ class HeadController(
         self.http_port = http_port
         self.node_port = node_port
         self.grpc_port = grpc_port
+        self.host_address = host_address
         self.head_node_address = f"{host_address}:{self.node_port}"
         self.proxy_manager = ProxyManager(self.node_port, self.http_port, self.grpc_port)
         self.node_info_cache = NodeInfo()
         self.auto_scale_manager = AutoScaleManager()
         # Sending autoscale manager to run autoscaling within deployment manager
         self.deployment_manager = DeploymentManager(self.node_info_cache, self.auto_scale_manager)   
-
+        #This queue holds information about the replicas that need to be created with the addresses of the workers
+        self.replica_queue = asyncio.Queue()
+        #This queue holds information about the replicas that need to be assigned to a worker
+        self.pending_replicas = asyncio.Queue()
+        #Start the replica creation task to run in the background
+        #self.vm_manager = VMManager("data-backup-457517-f5", "us-east1-d")
+        #Currently using AWSVMManager for AWS
+        self.vm_manager = AWSVMManager("us-east-1")
+        #Todo: See if this can be made better
+        asyncio.create_task(self.replica_creation())
         
-        
+       
         
         
         #self.deployment_scheduler = DeploymentScheduler(self)
         self.create_deployments()
-        self.health_manager = HealthManager(self.deployment_manager, self.node_info_cache)
+        #Need to call this in a thread, as VM creation is a blocking operation
+        asyncio.create_task(self._create_vm_in_thread())
+        
+        self.health_manager = HealthManager(self)
         
         
         self._node_map = {}
-        # Need to add autoscale manager here
-        #self.auto_scale_manager = AutoScaleManager() # Ensure this class is defined and imported
+        # Todo: Need to add autoscale manager here
+        #self.auto_scale_manager = AutoScaleManager() 
          
         # Shared variable for the latest routing table snapshot
         self._latest_routing_snapshot: Optional[headnode_service_pb2.RoutingUpdate] = None
@@ -54,22 +69,23 @@ class HeadController(
 
     def create_deployments(self):
         
-        self.deployment_manager.add_or_update_deployment(deployment_id = 1, name = "/v1/chat/gpt2", version = "1.0", num_cpus = 1, num_gpus = 0)
+        self.deployment_manager.add_or_update_deployment(deployment_id = 1, name = "/v1/chat/gpt2", version = "1.0", num_cpus = 2, num_gpus = 1)
         
-        self.deployment_manager.add_or_update_deployment(deployment_id = 2, name = "/v1/chat/tinyllama", version = "1.0", num_cpus = 1, num_gpus = 0)
+        self.deployment_manager.add_or_update_deployment(deployment_id = 2, name = "/v1/chat/tinyllama", version = "1.0", num_cpus = 2, num_gpus = 1)
        
-        self.deployment_manager.add_or_update_deployment(deployment_id = 3, name = "/v1/chat/phi2", version = "1.0", num_cpus = 1, num_gpus = 0)
+        self.deployment_manager.add_or_update_deployment(deployment_id = 3, name = "/v1/chat/phi2", version = "1.0", num_cpus = 2, num_gpus = 1)
+
         
-        self.deployment_manager.add_or_update_deployment(deployment_id = 4, name = "/v1/chat/TinyLlama", version = "1.0", num_cpus = 1, num_gpus = 0)
 
     async def _on_deployment_change(self):
         """Call this method after any change in DeploymentManager affects routing."""
         print("[HeadController] A deployment change occurred, generating new routing snapshot.")
         new_snapshot = await self.deployment_manager.get_current_routing_table()
-        async with self._routing_snapshot_lock: # Use consistent lock name
-            self._latest_routing_snapshot = new_snapshot # Use consistent variable name
+        async with self._routing_snapshot_lock:
+            self._latest_routing_snapshot = new_snapshot 
         
         self._routing_update_event.set()  # Signal subscribers
+
         self._routing_update_event.clear() # Reset for next signal
         print(f"[HeadController] Published new routing snapshot (ts: {new_snapshot.timestamp_ns if new_snapshot else 'N/A'}). Event processed.")
 
@@ -78,7 +94,7 @@ class HeadController(
                                         request: headnode_service_pb2.SubscriptionRequest, 
                                         context: grpc.aio.ServicerContext):
         proxy_id = request.proxy_id if request.proxy_id else "Unknown Proxy"
-        print(f"[HeadController] Proxy '{proxy_id}' subscribed for routing updates.")
+        print(f"[HeadController] Proxy  subscribed for routing updates.")
         #Start the heartbeat task of the proxy
         asyncio.create_task(self.proxy_manager.send_heartbeat())
         last_yielded_timestamp_ns = 0 
@@ -110,6 +126,7 @@ class HeadController(
                     print(f"[HeadController] Sending updated snapshot (ts: {new_snapshot.timestamp_ns}) to proxy '{proxy_id}'. Last sent ts: {last_yielded_timestamp_ns}.")
                     yield new_snapshot
                     last_yielded_timestamp_ns = new_snapshot.timestamp_ns
+                
                 # Else, this proxy already has this state or newer; or snapshot is None (should not happen if event set for valid state)
                 
         except asyncio.CancelledError:
@@ -125,10 +142,14 @@ class HeadController(
 
     # --- WorkerManagementServiceServicer RPCs ---
     
+    async def SendHealthStatus(self, request: headnode_service_pb2.HealthStatusUpdate, context):
+        print(f"[HeadController] Received health status update from worker {request.worker_id}")
+        # Delegate to health manager
+        return await self.health_manager.SendHealthStatus(request, context)
 
     async def RecordMetrics(self, request: headnode_service_pb2.MetricsRequest, context):
         #Send the metrics to the autoscaler
-        try:
+        try: 
             await self.auto_scale_manager.load_metrics(request)
             return headnode_service_pb2.MetricsReply(acknowledge=True)
         except Exception as e:
@@ -144,16 +165,32 @@ class HeadController(
         #Not sending update to the proxy yet- Need to create a replica
        
         worker_address = f"{request.node_address}:{request.port}"
-
-        asyncio.create_task(self.replica_creation(worker_address))
+        await self.replica_queue.put((worker_address, 2))
+        #await self.replica_queue.put((worker_address, 3))
+        #asyncio.create_task(self.replica_creation(worker_address))
         return headnode_service_pb2.RegisterReply(ack=True)
     
-    async def replica_creation(self, worker_address):
-        await asyncio.sleep(5)
-        print(f"[HeadController] Creating replica for deployment 1")
-        await self.create_replica(worker_address, 2, 1, 0)
-        print(f"[HeadController] Creating replica for deployment 2")
-        await self.create_replica(worker_address, 2, 1, 0)
+    async def replica_creation(self):
+        #Checks the queue for any replicas that need to be created
+        print(f"[HeadController] Starting replica creation task")
+        while True:
+            try:
+                replica_info = await self.replica_queue.get()
+                #Waiting for the node to start up, this is a hack to make sure the node is ready to receive requests
+                await asyncio.sleep(3)
+                
+                #This should never happen, if it does, need to fix it
+                if not isinstance(replica_info, tuple) or len(replica_info) != 2:
+                    print(f"[HeadController] Error: Invalid replica_info format: {replica_info}")
+                    continue
+                    
+                address, deployment_id = replica_info
+                await self.create_replica(address, deployment_id)
+                await asyncio.sleep(2)
+            except Exception as e:
+                print(f"[HeadController] Error in replica creation loop: {e}")
+                # Continue the loop even if there's an error
+                continue
 
     async def create_replica(self, address:str,deployment_id:int, _num_cpus = 0, _num_gpus = 0):
 
@@ -189,23 +226,44 @@ class HeadController(
                 await self._on_deployment_change()
             return response
         except Exception as e:
-            #Exact error
             
-            print(f"[HeadController] Error creating replica: {e}")
+            #Add this replica in a queue to be created later currently this can run forever
+            #Todo: but we can add a timeout to the request to be created later.
+            print(f"[HeadController] Error creating replica for deployment {deployment_id}: Will retry later")
+            await self.pending_replicas.put((deployment_id))
             return None
         
 
     
+    async def _create_vm_in_thread(self):
+        """Run VM creation in a separate thread using asyncio.to_thread."""
+        for i in range(2):
+            try:
+                print(f"[HeadController] Creating VM {i+1}")
+                await asyncio.to_thread(
+                    self.vm_manager.create_vm,
+                    scheduler_id=i+1,
+                    name=f"scheduler-{i+1}",
+                    model_id="small",
+                    head_controller_address=self.host_address,
+                    head_controller_port=self.node_port
+                )
+            except Exception as e:
+                print(f"[HeadController] Error creating VM in thread: {e}")
+            await asyncio.sleep(5)
+       
+
 async def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host_address", type=str, required=False, default="localhost")
     parser.add_argument("--http_port", type=int, required=True)
     parser.add_argument("--node_port", type=int, required=True)
     parser.add_argument("--grpc_port", type=int, required=True)
 
    
     args = parser.parse_args()
-    head_controller_instance = HeadController(args.http_port, args.node_port, args.grpc_port, args.host_address)
+    host_address = get_aws_internal_ip() 
+    #host_address =get_local_ip()
+    head_controller_instance = HeadController(args.http_port, args.node_port, args.grpc_port, host_address)
    
     server = grpc.aio.server()
     headnode_service_pb2_grpc.add_WorkerManagementServiceServicer_to_server(
